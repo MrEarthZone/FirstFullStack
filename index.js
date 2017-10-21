984e786f2b4b971380da6290ec80a96bb8d8bdbe
[Sirom] <[sirom2010@gmail.com]>
2017-10-22 00:59:27 +0700

    first stage finish

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    var chack = 0;
     //register
     $("#signup").click(function () {
         $.post("http://localhost:3000/regis", {
@@ -7,8 +8,26 @@ $(document).ready(function () {
             username: $("#user").val(),
             password: $("#pass").val()
         });
-        console.log($("#name").val()+" Complete");
+        console.log($("#name").val() + " Complete");
     });
     //login
-    
+    $("#login").click(function () {
+        $.getJSON("http://localhost:3000/regis", function (data) {
+            for (i = 0; i < data.length; i++) {
+                if (data[i].username == $("#name_login").val() && data[i].password == $("#pass_login").val()) {
+                    localStorage.setItem("id", data[i].id);
+                    window.location = "pageHome.html";
+                    chack++;
+                }
+                if (i == data.length - 1 && chack == 0) {
+                    alert("Username or Password your are wrong !!");
+                }
+            }
+        });
+    });
+    //logout
+    $("#logout").click(function () {
+        localStorage.removeItem("id");
+    });
+    console.log(location.protocol);
 });
